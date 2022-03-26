e91c3ab046a940d205d31e12475aba270cf9e35d
Alexander Kostarev <UniverAlex@ya.ru>
2022-03-26 14:05:41 +0300

    added slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,13 +1,13 @@
-var totalImages = 5;
-
+const totalImages = 124;
 function shiftImage(x) {
-	var imageSource =  document.getElementById('categories').src;
-	var imageNumber =  parseInt(imageSource.match(/\d/)[0]) + x;
-	if(imageNumber == 0) {
+	const galleryImage = document.getElementById("gallery-image");
+	const imagePath = getComputedStyle(galleryImage).backgroundImage;
+	var imageNumber = parseInt(imagePath.match(/(\d*)\.jpg/)[1]) + x;
+
+	if (imageNumber === 0) {
 		imageNumber = totalImages;
-	} else if(imageNumber == totalImages + 1){
+	} else if (imageNumber == totalImages + 1) {
 		imageNumber = 1;
 	}
-	document.getElementById('categories').src = imageSource.replace(/\d/, imageNumber);
-	console.log(document.getElementById('categories').src);
+	galleryImage.style.backgroundImage = imagePath.replace(/\d*\.jpg/, imageNumber + ".jpg");
 }
